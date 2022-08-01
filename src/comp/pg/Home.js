01cdadbe4edf73d4007d7442ca2060482fa0b9d5
M1racle04 <yashku77@gmail.com>
2022-08-02 05:01:54 +0530

    frist commit

---
+++ src/comp/pg/Home.js
@@ -0,0 +1,77 @@
+
+import "./button.css";
+import "./Footer.css";
+
+
+const year = new Date().getFullYear();
+
+const Home = ( ) => {
+    return <>
+    
+    
+    <div class="hero-image">
+  
+   
+  </div>
+
+
+  <section id= "detailes">
+
+<h2>WELCOME TO THE REALM OF CHAMPIONS</h2>
+<p class="pa">Do You Have What it Takes To be on the TOP? </p>
+<p class="pa">If you think you have what it takes to be called as champions , then we give you the grand stage to compete and show it to the world.</p>
+<p class="pa">REGISTER today and participate in the most fierce battle ever. </p>
+<h2 className="">Participate Today and WIN prizes worth THOUSANDS!!!!</h2>
+
+
+       
+
+    <div class="reg">
+  
+
+    <a href="https://forms.gle/1FvsPz8vzkWQ261EA" target="blank">
+    <button  class="btn btn--light body"  >
+      <span class="btn__inner">
+        <span class="btn__slide"></span>
+        <span class="btn__content">Register NOW!</span>
+      </span>
+    </button>
+    </a>
+  </div>
+
+
+  
+
+
+  </section> 
+
+
+
+
+
+  <div class=" container-fluid1 mainimg">
+ 
+</div>
+    
+
+
+    
+
+ 
+
+ <footer id="footer">
+      <p class="footes">Copyright METHGAMING ⓒ {year} </p>
+    </footer>
+    
+    </>
+
+
+
+   
+    
+    
+    
+    
+    }
+    
+    export default Home;
